9989765f99087b1651e70b1d15bdeabfe279c2c3
dci-student <alinacuznetov@gmail.com>
2022-07-06 12:17:26 +0200

    operators

---
+++ solution.js
@@ -0,0 +1,33 @@
+// 1 a
+
+
+console.log (3 == "3")
+console.log (3 === "3")
+
+// 1b 
+
+2. ==  
+
+// 1c 
+
+7. === 
+
+// 1d 
+
+9. =
+
+
+// 2 
+
+let myVar = true; 
+console.log ( myVar!=true ? 'good morning' : 'good evening'); 
+
+// 3 
+
+
+let firstName = '' .... 'John' .... 0
+let emptyStr  = '' .... false .... 'Hello World'
+let zero  = '' .... false .... 0
+let seven = 75 .... 'nine' .... 7
+
+console.log(firstName,zero,emptyStr,seven);
